3f8183150d7d2e27382a7baaf115504cb1d908f0
rmounier <romain.mounier@univ-rennes.fr>
2024-02-06 15:58:18 +0100

    Reset MainActivity

--- app/src/main/java/com/ihealth/demo/business/MainActivity.java
+++ app/src/main/java/com/ihealth/demo/business/MainActivity.java
@@ -1,35 +1,16 @@
 package com.ihealth.demo.business;
 
-import android.Manifest;
-import android.widget.FrameLayout;
+import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.ihealth.demo.R;
-import com.ihealth.demo.base.BaseApplication;
 /**
  * MainActivity
- * Containers for all fragment
  */
 public class MainActivity extends AppCompatActivity {
-    FrameLayout mFlContent;
-
-    public int contentViewID() {
-        return R.layout.activity_main;
-    }
-
-    public void initView() {
-        init();
-    }
-
-    private void init() {
-    }
-
-
-
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        BaseApplication.instance().logOut();
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 }
