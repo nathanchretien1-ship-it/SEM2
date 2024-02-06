b4af1284864a4a6afafe9bc9cf7e56286514a507
rmounier <romain.mounier@univ-rennes.fr>
2024-02-06 18:48:50 +0100

    Add SDK authentication

--- app/src/main/java/com/ihealth/demo/base/BaseApplication.java
+++ app/src/main/java/com/ihealth/demo/base/BaseApplication.java
@@ -5,6 +5,9 @@ import android.util.Log;
 
 import com.ihealth.communication.manager.iHealthDevicesManager;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 public class BaseApplication extends Application {
 
     @Override
@@ -19,6 +22,21 @@ public class BaseApplication extends Application {
          * and connect these devices through iHealthDevicesManager.
          */
         iHealthDevicesManager.getInstance().init(this, Log.VERBOSE, Log.VERBOSE);
+
+
+        try {
+            InputStream is = getAssets().open("com_demo_sdk_android.pem");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            boolean isPass = iHealthDevicesManager.getInstance().sdkAuthWithLicense(buffer);
+            Log.d("SEM2", "isPass: " + isPass);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
 }
