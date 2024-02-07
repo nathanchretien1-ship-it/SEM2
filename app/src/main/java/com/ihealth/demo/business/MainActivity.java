be9d30dd202fa9237b8724e84b991ff440c1a3db
rmounier <romain.mounier@univ-rennes.fr>
2024-02-07 11:39:09 +0100

    Removed BaseApplication Removed SDK Authentification

--- app/src/main/java/com/ihealth/demo/business/MainActivity.java
+++ app/src/main/java/com/ihealth/demo/business/MainActivity.java
@@ -1,10 +1,12 @@
 package com.ihealth.demo.business;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.ihealth.communication.manager.iHealthDevicesManager;
 /**
  * MainActivity
  */
@@ -12,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        /*
+         * Initializes the iHealth devices manager. Can discovery available iHealth devices nearby
+         * and connect these devices through iHealthDevicesManager.
+         */
+        iHealthDevicesManager.getInstance().init(getApplication(), Log.VERBOSE, Log.VERBOSE);
+
     }
 }
